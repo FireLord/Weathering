3e6d2a3ab8e98375ca970cc67cc032f38d8006d6
FireLord <firelord.xda@gmail.com>
2020-07-09 10:59:45 +0530

    weathering: Only for testing
    Signed-off-by: FireLord <firelord.xda@gmail.com>

--- app/src/main/java/com/firelord/weathering/MainActivity.kt
+++ app/src/main/java/com/firelord/weathering/MainActivity.kt
@@ -2,10 +2,22 @@ package com.firelord.weathering
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.firelord.weathering.data.OpenWeatherServiceApi
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.IO
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val apiService = OpenWeatherServiceApi()
+        GlobalScope.launch(Dispatchers.IO) {
+            val currentWeatherResponse = apiService.getCurrentWeather("Noida").await()
+            tvTemp.text=currentWeatherResponse.main.toString()
+        }
     }
 }
