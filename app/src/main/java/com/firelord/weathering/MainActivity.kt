9989f55d7b625e2620da30706adcd7448228475b
Shubham Singh <coolsks94@gmail.com>
2020-07-09 14:22:12 +0530

    Fix crash while network call (#1)
    * Remove jake wharton's coroutines adapter as its deprecated and retrofit supports coroutines by default
    * Enable data binding
    * Update data on view on UI thread

--- app/src/main/java/com/firelord/weathering/MainActivity.kt
+++ app/src/main/java/com/firelord/weathering/MainActivity.kt
@@ -1,23 +1,35 @@
 package com.firelord.weathering
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.firelord.weathering.data.OpenWeatherServiceApi
-import kotlinx.android.synthetic.main.activity_main.*
+import com.firelord.weathering.databinding.ActivityMainBinding
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.IO
 import kotlinx.coroutines.launch
+import retrofit2.awaitResponse
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mBinding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         val apiService = OpenWeatherServiceApi()
         GlobalScope.launch(Dispatchers.IO) {
-            val currentWeatherResponse = apiService.getCurrentWeather("Noida").await()
-            tvTemp.text=currentWeatherResponse.main.toString()
+            apiService.getCurrentWeather("Noida").awaitResponse()
+                .run {
+                    if(isSuccessful) {
+                        body()?.let {
+                            runOnUiThread {
+                                mBinding.tvTemp.text = it.main.temp.toString()
+                            }
+                        }
+                    }
+                }
         }
     }
 }
