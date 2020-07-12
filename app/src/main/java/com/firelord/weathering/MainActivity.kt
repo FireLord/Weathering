68fe87a2a6906874c69b7006eaaa1a2ff287ef3e
FireLord <firelord.xda@gmail.com>
2020-07-13 00:48:04 +0530

    app: Fix constraintlayout using helpers
    * thanks @ishubhamsingh for example
    * added material library
    * dont use data binding
    
    Signed-off-by: FireLord <firelord.xda@gmail.com>

--- app/src/main/java/com/firelord/weathering/MainActivity.kt
+++ app/src/main/java/com/firelord/weathering/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.firelord.weathering
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
@@ -10,6 +11,7 @@ import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import retrofit2.awaitResponse
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,14 +23,17 @@ class MainActivity : AppCompatActivity() {
         val view = mainActivity.root
         setContentView(view)
 
-        mainActivity.button.setOnClickListener {
+        mainActivity.buReport.setOnClickListener {
             lifecycleScope.launch {
                 getApi()
             }
         }
+        supportActionBar?.elevation = 0F
     }
 
     val apiService = OpenWeatherServiceApi()
+
+    @SuppressLint("SetTextI18n")
     suspend fun getApi() {
         withContext(IO) {
             val city = mainActivity.etLocation.text.toString()
@@ -37,7 +42,15 @@ class MainActivity : AppCompatActivity() {
                     if (isSuccessful) {
                         body()?.let {
                             withContext(Dispatchers.Main) {
-                                mainActivity.tvTemp.text = it.main.temp.toString()
+                                mainActivity.tvTemp.text = "${it.main.temp.toInt()}°C"
+                                mainActivity.tvRain.text = "${it.clouds.all}%"
+                                mainActivity.tvHumidity.text = "${it.main.humidity}%"
+                                mainActivity.tvWind.text = "${it.wind.deg}" //TODO take deg change to sw/e/n
+                                mainActivity.tvDay.text = "Sunday" //TODO get it from system
+                                mainActivity.tvDate.text = "${Calendar.getInstance()
+                                    .get(Calendar.DATE)}/${Calendar.getInstance()
+                                    .get(Calendar.MONTH)}/${Calendar.getInstance()
+                                    .get(Calendar.YEAR)}"
                             }
                         }
                     }
