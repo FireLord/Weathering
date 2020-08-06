3c74958e912f5ebb0e0a084bfd85b7b672f10da0
Harsh Shandilya <me@msfjarvis.dev>
2020-08-06 17:39:43 +0530

    gh-actions: Switch to release builds
    Signed-off-by: FireLord <firelord.xda@gmail.com>

---
+++ release/signing-cleanup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -ex
+
+# Delete Release key
+rm -f keystore
+
+# Delete signing config
+rm -f keystore.properties
---
+++ release/signing-setup.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -ex
+
+ENCRYPT_KEY=$1
+
+declare -A SECRETS
+SECRETS[release/keystore.cipher]=keystore
+SECRETS[release/props.cipher]=keystore.properties
+
+if [[ -n "$ENCRYPT_KEY" ]]; then
+    for src in "${!SECRETS[@]}"; do
+      openssl enc -aes-256-cbc -md sha256 -pbkdf2 -d -in "${src}" -out "${SECRETS[${src}]}" -k "${ENCRYPT_KEY}"
+    done
+else
+    echo "Usage: ./signing-setup.sh <encryption key>"
+fi
