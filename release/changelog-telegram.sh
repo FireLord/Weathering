9284fabd18e24dc56ed6b6b120e58cd34717af6d
FireLord <firelord.xda@gmail.com>
2020-08-10 12:15:25 +0530

    gh-actions: Send changelog in code text style
    [ci skip]
    
    Signed-off-by: FireLord <firelord.xda@gmail.com>

--- release/changelog-telegram.sh
+++ release/changelog-telegram.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 function gitFun() {
-    git log --pretty="format:%s" -1
-    echo -e "\n"
-    git log --pretty="format:%b" -1
+  echo '```' 
+  git log --pretty="format:%s" -1
+  echo -e "\n"
+  git log --pretty="format:%b" -1
+  echo '```'
 }
 logMsg=$(gitFun)
 
@@ -15,4 +17,4 @@ function changelog_tg() {
   curl -F chat_id="${CHAT_ID}" -F text="${MSG}" -F parse_mode="Markdown" "https://api.telegram.org/bot${TG_TOKEN:?}/sendMessage" >/dev/null 2>&1
 }
 
-changelog_tg "${TG_TO:?}" "${logMsg}"
+changelog_tg "${TG_TO:?}" "${logMsg:?}"
