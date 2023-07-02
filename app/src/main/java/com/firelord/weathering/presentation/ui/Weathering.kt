58e60dc8de987043e5e05ddb5b182ff7c46b7438
FireLord <firelord.xda@gmail.com>
2023-07-02 17:55:34 +0530

    app: Setup dark mode

--- app/src/main/java/com/firelord/weathering/presentation/ui/Weathering.kt
+++ app/src/main/java/com/firelord/weathering/presentation/ui/Weathering.kt
@@ -1,7 +1,10 @@
 package com.firelord.weathering.presentation.ui
 
 import android.app.Application
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.PreferenceManager
 import com.firelord.weathering.data.Constants
 import com.firelord.weathering.core.data.Preferences
 import dagger.hilt.android.HiltAndroidApp
@@ -11,11 +14,24 @@ class Weathering : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        AppCompatDelegate.setDefaultNightMode(
-            Preferences(this).fetchInt(
-                Constants.KEY_THEME_MODE,
-                AppCompatDelegate.MODE_NIGHT_NO
-            )
-        )
+        applyAppTheme()
+    }
+
+    private fun applyAppTheme() {
+        val sharedPreferences: SharedPreferences =
+            PreferenceManager.getDefaultSharedPreferences(this)
+        val themeValue = sharedPreferences.getString("app_theme", "system")
+
+        when (themeValue) {
+            "dark" -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }
+            "light" -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+            "system" -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+            }
+        }
     }
 }
--- app/src/main/java/com/firelord/weathering/presentation/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/firelord/weathering/presentation/ui/settings/SettingsFragment.kt
@@ -1,12 +1,46 @@
 package com.firelord.weathering.presentation.ui.settings
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.ListPreference
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceManager
 import com.firelord.weathering.R
+
 
 class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
+
+        val themePreference: ListPreference? = findPreference("darkMode")
+        themePreference?.onPreferenceChangeListener =
+            Preference.OnPreferenceChangeListener { _, newValue ->
+                val themeValue = newValue as String
+                val sharedPreferences =
+                    PreferenceManager.getDefaultSharedPreferences(requireContext())
+                sharedPreferences.edit().putString("app_theme", themeValue).apply()
+                applyAppTheme(themeValue)
+                true
+            }
+    }
+    private fun applyAppTheme(themeValue: String) {
+        when (themeValue) {
+            "dark" -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }
+
+            "light" -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+
+            "system" -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+            }
+        }
+        activity?.recreate()
     }
 }
