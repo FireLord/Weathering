378be48e0a7ec87f931c0df672a789fd37382cda
FireLord <firelord.xda@gmail.com>
2024-01-14 00:12:11 +0530

    release: Rename file with version

--- release/deploy-telegram.sh
+++ release/deploy-telegram.sh
@@ -10,4 +10,16 @@ function send_to_tg() {
   curl -F chat_id="${CHAT_ID}" -F document="@${FILE}" -F caption="${CAPTION}" -F parse_mode="Markdown" "https://api.telegram.org/bot${TG_TOKEN:?}/sendDocument" >/dev/null 2>&1
 }
 
-send_to_tg "${TG_FILE:?}" "${TG_TO:?}" "weathering-alpha-${GITHUB_RUN_NUMBER}-release"
+# Extract versionCode and versionName from build.gradle
+versionCode=$(grep "versionCode" app/build.gradle | awk '{print $2}' | tr -d '\r')
+versionName=$(grep "versionName" app/build.gradle | awk '{print $2}' | tr -d '\r')
+
+# Set the source and destination paths
+source_file="${TG_FILE:?}"
+destination_path="app/build/outputs/apk/release/Weathering${versionCode}(${versionName}).apk"
+
+# Rename the file (providing full path for source)
+mv "${source_file}" "${destination_path}"
+
+# Send to Telegram
+send_to_tg "${destination_path}" "${TG_TO:?}" "weathering-alpha-${GITHUB_RUN_NUMBER}-release"
